6b64c54f2d82a0b0235235badfe84eee9ed9bb42
mrmshor <shorja2@gmail.com>
2025-08-06 14:50:17 +0300

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_plugin_shell::ShellExt;
-
-#[tauri::command]
-async fn open_folder(app: tauri::AppHandle, path: String) -> Result<(), String> {
-    app.shell()
-        .open(path, None)
-        .map_err(|e| format!("Failed to open folder: {}", e))?;
-    Ok(())
-}
-
-#[tauri::command]
-async fn get_desktop_path() -> Result<String, String> {
-    let home_dir = dirs::home_dir()
-        .ok_or("Could not find home directory")?;
-    
-    let desktop_path = home_dir.join("Desktop");
-    
-    Ok(desktop_path.to_string_lossy().to_string())
-}
-
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![open_folder, get_desktop_path])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
